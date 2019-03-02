fb39759b8338bd5edf9d5f130a13ab05e8a61ceb
James Gallagher <james@Opencommit.com>
2019-03-02 03:17:45 -0800

    feat: Create new LeaderboardContainer

--- app/src/containers/LeaderboardContainer/index.js
+++ app/src/containers/LeaderboardContainer/index.js
@@ -1,86 +1,141 @@
-import React, { Component } from 'react';
-import Box from 'grommet/components/Box';
-import Section from 'grommet/components/Section';
-import Hero from 'grommet/components/Hero';
-import Heading from 'grommet/components/Heading';
-import Image from 'grommet/components/Image';
-import Anchor from 'grommet/components/Anchor';
-import Table from 'grommet/components/Table';
-import TableRow from 'grommet/components/TableRow';
-import Columns from 'grommet/components/Columns';
+import React, { Component } from "react";
+import Box from "grommet/components/Box";
+import Heading from "grommet/components/Heading";
+import Section from "grommet/components/Section";
+import FormField from "grommet/components/FormField";
+import Footer from "grommet/components/Footer";
+import Select from "grommet/components/Select";
+import Columns from "grommet/components/Columns";
+import CheckmarkIcon from "grommet/components/icons/base/Checkmark";
+import Button from "grommet/components/Button";
+import Menu from "grommet/components/Menu";
+import Table from "grommet/components/Table";
+import TableRow from "grommet/components/TableRow";
+import { graphql, compose } from "react-apollo";
+import Toast from "grommet/components/Toast";
+import axios from "axios";
+import gql from "graphql-tag";
+import styles from "./index.module.scss";
+import { FullSection, MainContent, MainBox } from "./styles";
+import { Divider, LoadingIndicator, SettingsSidebar } from "components";
+import { Navbar, AppFooter } from "components";
+import regeneratorRuntime from "regenerator-runtime";
 import Highcharts from 'highcharts/highstock';
 import HighchartsReact from 'highcharts-react-official';
-import axios from 'axios';
-import { MainBox, FullSection } from './styles';
-import { LoadingIndicator } from 'components';
-import { Navbar, AppFooter } from "components";
-import styles from './index.module.scss';
 
-class LeaderboardContainer extends Component {
+class ShareholdersContainer extends Component {
   constructor() {
     super();
     this.state = {
-      questions: '',
+      token: null,
+      updateAccountToast: false,
+      load_default: false,
       getData: false,
-      most_shares: [],
-      all_shares: [],
+      getSecondData: false,
+      currentUser: "None",
+      isLoading: true,
+      investments: []
     };
   }
-  render() {
+
+  componentDidMount() {
+    if (!localStorage.getItem("auth_token")) {
+      window.location.replace("/login");
+    }
     if (this.state.getData === false) {
       const axiosGitHubGraphQL = axios.create({
-        baseURL: `${process.env.NODE_ENV === 'development' ? 'https://jamesg.herokuapp.com/graphql' : 'https://api.jamesg.app/graphql'}`,
+        baseURL: `${
+          process.env.NODE_ENV === "development"
+            ? "https://jamesg.herokuapp.com/graphql"
+            : "https://api.jamesg.app/graphql"
+        }`,
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("auth_token")}`
+        }
       });
 
-      const MAIN_QUERY = `{ getHighestBalance(limit: 10) { id name balance work_badge friend_badge } }`;
+      const MAIN_QUERY = `{ getCurrentUser { id name role email profile_picture bio position location twitter personal_website gender api_key shares_issued } }`;
 
       axiosGitHubGraphQL
-        .post(`${process.env.NODE_ENV === 'development' ? 'https://jamesg.herokuapp.com/graphql' : 'https://api.jamesg.app/graphql'}`, { query: MAIN_QUERY })
+        .post(
+          `${
+            process.env.NODE_ENV === "development"
+              ? "https://jamesg.herokuapp.com/graphql"
+              : "https://api.jamesg.app/graphql"
+          }`,
+          { query: MAIN_QUERY }
+        )
         .then(result => {
           this.setState({
-            highest_balance: result.data.data.getHighestBalance,
-            getData: true,
+            currentUser: result.data.data.getCurrentUser[0],
           });
         })
-        .catch(error => {
-          this.setState({ getData: true });
+        .catch(result => {
+          this.setState({
+            currentUser: "None",
+          });
         });
 
-      const SECOND_QUERY = `{ getMostShares(limit: 10) { id name shares work_badge friend_badge } }`;
+      const SHAREHOLDERS_QUERY = `{ getShareholders { id name shares balance }`;
 
       axiosGitHubGraphQL
-        .post(`${process.env.NODE_ENV === 'development' ? 'https://jamesg.herokuapp.com/graphql' : 'https://api.jamesg.app/graphql'}`, { query: SECOND_QUERY })
+        .post(
+          `${
+            process.env.NODE_ENV === "development"
+              ? "https://jamesg.herokuapp.com/graphql"
+              : "https://api.jamesg.app/graphql"
+          }`,
+          { query: SHAREHOLDERS_QUERY }
+        )
         .then(result => {
           this.setState({
-            most_shares: result.data.data.getMostShares,
+            shareholders: result.data.data.getShareholders,
+            getSecondData: true,
             getData: true,
+            isLoading: false
           });
         })
-        .catch(error => {
-          this.setState({ getData: true });
-        });
-
-      const THIRD_QUERY = `{ getUsers(limit: 2000) { id name shares work_badge friend_badge } }`;
-
-      axiosGitHubGraphQL
-        .post(`${process.env.NODE_ENV === 'development' ? 'https://jamesg.herokuapp.com/graphql' : 'https://api.jamesg.app/graphql'}`, { query: THIRD_QUERY })
-        .then(result => {
+        .catch(result => {
           this.setState({
-            all_shares: result.data.data.getUsers,
+            shareholders: [],
             getData: true,
+            isLoading: false
           });
-        })
-        .catch(error => {
-          this.setState({ getData: true });
         });
+        const MAIN_QUERY = `{ getHighestBalance(limit: 10) { id name balance work_badge friend_badge } }`;
+
+        axiosGitHubGraphQL
+          .post(`${process.env.NODE_ENV === 'development' ? 'https://jamesg.herokuapp.com/graphql' : 'https://api.jamesg.app/graphql'}`, { query: MAIN_QUERY })
+          .then(result => {
+            this.setState({
+              highest_balance: result.data.data.getHighestBalance,
+              getData: true,
+            });
+          })
+          .catch(error => {
+            this.setState({ getData: true });
+          });
+
+        const SECOND_QUERY = `{ getMostShares(limit: 10) { id name shares work_badge friend_badge } }`;
+
+        axiosGitHubGraphQL
+          .post(`${process.env.NODE_ENV === 'development' ? 'https://jamesg.herokuapp.com/graphql' : 'https://api.jamesg.app/graphql'}`, { query: SECOND_QUERY })
+          .then(result => {
+            this.setState({
+              most_shares: result.data.data.getMostShares,
+              getData: true,
+            });
+          })
+          .catch(error => {
+            this.setState({ getData: true });
+          });
     }
+  }
 
-    if (this.state.most_shares.length === 0) {
-      return (
-        <div />
-      );
+  render() {
+    if (this.state.isLoading === true) {
+      return <div />;
     }
-
     var data = this.state.most_shares.map(function(share) {
       return { name: share.name, y: share.shares };
     });
@@ -93,7 +148,7 @@ class LeaderboardContainer extends Component {
       return share.shares;
     });
 
-    var total = parseFloat(100000) - parseFloat(shareTotal[0]);
+    var total = this.state.currentUser.shares_issued - parseFloat(shareTotal[0]);
 
     data.push({ name: 'Other', y: total });
 
@@ -131,104 +186,91 @@ class LeaderboardContainer extends Component {
 
     return (
       <div>
-        <Navbar pathname={this.props.props.pathname} />
-      <Box className={styles.container}>
-        <Box full="horizontal">
-          <Hero
-            justify="center"
-            align="center"
-            size="small"
-            backgroundColorIndex="dark"
-            background={
-              <Image
-                fit="cover"
-                full={true}
-                className={styles.heroImage}
-                src="https://images.unsplash.com/photo-1543532224-f27c14ca2540?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=51e7328d6ab80920a095bdea293ace91&auto=format&fit=crop&w=1400&q=80"
-              />
-            }
-          >
-            <Box align="center" justify="center">
-              <Heading align="center" tag="h1">
-                Leaderboard
+        <MainBox alignContent="center" fill="horizontal" align="center">
+          <FullSection primary direction="row">
+            <SettingsSidebar currentUser={this.state.currentUser} />
+            <MainContent
+              align="center"
+              justify="start"
+              pad={{ vertical: "large" }}
+            >
+              <Heading tag="h2" align="center">
+                Shareholders
               </Heading>
-            </Box>
-          </Hero>
-          <Section align="center" justify="center">
-            <HighchartsReact highcharts={Highcharts} options={options} />
-            <Columns size="large" align="center" justify="center">
-              <Box align="left" pad="medium" margin="small" pad="large">
-                <Heading align="center" tag="h2">
-                  Most Shares
-                </Heading>
-                {this.state.most_shares && (
-                  <div>
-                    <Table>
-                      <thead>
-                        <tr>
-                          <th>User</th>
-                          <th>Shares</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        {this.state.most_shares.map(user => {
-                          return (
-                            <TableRow>
-                              <td>
-                                <Anchor
-                                  href={`/profile/${user.id}`}
-                                  label={`${user.name} ${user.work_badge == true ? '💼' : '' } ${user.friend_badge == true ? '🙌' : '' }`}
-                                />
-                              </td>
-                              <td>{user.shares}</td>
-                            </TableRow>
-                          );
-                        })}
-                      </tbody>
-                    </Table>
-                  </div>
-                )}
-              </Box>
-              <Box align="left" pad="medium" margin="small" pad="large">
-                {this.state.highest_balance && (
-                  <div>
+
+                <HighchartsReact highcharts={Highcharts} options={options} />
+                <Columns size="large" align="center" justify="center">
+                  <Box align="left" pad="medium" margin="small" pad="large">
                     <Heading align="center" tag="h2">
-                      Highest Balance
+                      Most Shares
                     </Heading>
-                    <Table>
-                      <thead>
-                        <tr>
-                          <th>User</th>
-                          <th>Balance</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        {this.state.highest_balance.map(user => {
-                          return (
-                            <TableRow>
-                              <td>
-                                <Anchor
-                                  href={`/profile/${user.id}`}
-                                  label={`${user.name} ${user.work_badge == true ? '💼' : '' } ${user.friend_badge == true ? '🙌' : '' }`}
-                                />
-                              </td>
-                              <td>${parseFloat(user.balance).toFixed(2)}</td>
-                            </TableRow>
-                          );
-                        })}
-                      </tbody>
-                    </Table>
-                  </div>
-                )}
-              </Box>
-            </Columns>
-          </Section>
-        </Box>
-      </Box>
-      <AppFooter />
-    </div>
+                    {this.state.most_shares && (
+                      <div>
+                        <Table>
+                          <thead>
+                            <tr>
+                              <th>User</th>
+                              <th>Shares</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            {this.state.most_shares.map(user => {
+                              return (
+                                <TableRow>
+                                  <td>
+                                    <Anchor
+                                      href={`/profile/${user.id}`}
+                                      label={`${user.name}`}
+                                    />
+                                  </td>
+                                  <td>{user.shares}</td>
+                                </TableRow>
+                              );
+                            })}
+                          </tbody>
+                        </Table>
+                      </div>
+                    )}
+                  </Box>
+                  <Box align="left" pad="medium" margin="small" pad="large">
+                    {this.state.highest_balance && (
+                      <div>
+                        <Heading align="center" tag="h2">
+                          Highest Balance
+                        </Heading>
+                        <Table>
+                          <thead>
+                            <tr>
+                              <th>User</th>
+                              <th>Balance</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            {this.state.highest_balance.map(user => {
+                              return (
+                                <TableRow>
+                                  <td>
+                                    <Anchor
+                                      href={`/profile/${user.id}`}
+                                      label={`${user.name}`}
+                                    />
+                                  </td>
+                                  <td>${parseFloat(user.balance).toFixed(2)}</td>
+                                </TableRow>
+                              );
+                            })}
+                          </tbody>
+                        </Table>
+                      </div>
+                    )}
+                  </Box>
+                </Columns>
+            </MainContent>
+          </FullSection>
+        </MainBox>
+      </div>
     );
   }
 }
 
-export default LeaderboardContainer;
+export default ShareholdersContainer;
